a0b8a167fa4ce87523a4e0da52ddb5443ed7bce2
liaoaoyuan97 <al3468@columbia.edu>
2016-02-13 10:43:41 -0500

    rawdata

---
+++ patient.R
@@ -0,0 +1,19 @@
+setwd("C:/Original/good stat/Data Hackathon")
+library("dplyr")
+library("data.table")
+library("ggplot2")
+library("choroplethr")
+
+reRead <- 1
+##read data and save it as RData to save time nect time:
+if(reRead==1){
+  colsToKeep <- c("Facility Id")
+  Data <- fread("Hospital_Inpatient_Discharges__SPARCS_De-Identified___2012.csv", select=colsToKeep )  
+  save(Data, file="rawData.RData")
+}else{
+  load("rawData.RData")
+} 
+
+data<-tbl_df(Data)
+dm<-na.omit(data) %>%
+    group_by(`Facility Id`)
